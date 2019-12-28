168ae8e3be4c31b10f23ecb4929b4c74fe00cc47
Francisco Laborde <flabordec@gmail.com>
2019-12-28 15:56:30 -0600

    Adding web UI.

---
+++ ComeAndTicketWebUi/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MaguSoft.ComeAndTicket.Core.Model;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ComeAndTicketWebUi
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MaguSoft.ComeAndTicket.Core.Model.ComeAndTicketContext _context;
+
+        public DetailsModel(MaguSoft.ComeAndTicket.Core.Model.ComeAndTicketContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+        public IList<ShowTime> ShowTimes { get; set; }
+        public IList<ShowTime> FilteredShowTimes { get; set; }
+        public IList<string> Markets { get; set; }
+        public string MarketFilter { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id, string marketFilter)
+        {
+            if (string.IsNullOrEmpty(marketFilter))
+                marketFilter = "Austin";
+
+            MarketFilter = marketFilter;
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies
+                .Include(m => m.ShowTimes)
+                    .ThenInclude(st => st.Theater)
+                        .ThenInclude(t => t.Market)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Title == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+
+            ShowTimes = Movie.ShowTimes
+                .Where(st => 
+                    st.Date >= DateTime.UtcNow && 
+                    st.SeatsLeft > 0)
+                .ToList();
+            Markets = ShowTimes
+                .Select(st => st.Theater.Market.Name)
+                .Distinct()
+                .ToList();
+            if (marketFilter.Equals("all-movies"))
+            {
+                FilteredShowTimes = ShowTimes;
+            }
+            else 
+            { 
+                FilteredShowTimes = ShowTimes
+                    .Where(st => st.Theater.Market.Name.Contains(marketFilter, StringComparison.CurrentCultureIgnoreCase))
+                    .ToList();
+            }
+            return Page();
+        }
+    }
+}
---
+++ ComeAndTicketWebUi/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MaguSoft.ComeAndTicket.Core.Model;
+
+namespace ComeAndTicketWebUi
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MaguSoft.ComeAndTicket.Core.Model.ComeAndTicketContext _context;
+
+        public IndexModel(MaguSoft.ComeAndTicket.Core.Model.ComeAndTicketContext context)
+        {
+            _context = context;
+        }
+
+        public string TitleFilter { get; set; }
+
+        public string TitleSort { get; set; }
+
+        public IList<Movie> Movies { get;set; }
+
+        public async Task OnGetAsync(string sortOrder, string titleFilter)
+        {
+            TitleFilter = titleFilter;
+
+            IQueryable<Movie> query = _context.Movies
+                .Include(m => m.ShowTimes)
+                .AsNoTracking();
+            switch (sortOrder)
+            {
+                case null:
+                case "title":
+                    TitleSort = "title_desc";
+                    query = query.OrderBy(m => m.Title);
+                    break;
+                case "title_desc":
+                    TitleSort = "title";
+                    query = query.OrderByDescending(m => m.Title);
+                    break;
+            }
+
+            query = query.Where(m => m.ShowTimes.Any(st => st.Date >= DateTime.UtcNow && st.SeatsLeft > 0));
+
+            var moviesPreFilter = await query.ToListAsync();
+            if (!string.IsNullOrEmpty(titleFilter))
+            {
+                Movies = moviesPreFilter.Where(m => m.Title.Contains(titleFilter, StringComparison.CurrentCultureIgnoreCase)).ToList();
+            }
+            else
+            {
+                Movies = moviesPreFilter;
+            }
+        }
+    }
+}
